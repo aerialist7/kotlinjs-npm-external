eac2ebb0f3fabf3e4a76159cbedcfe87bd73b95e
aerialist7 <32072831+aerialist7@users.noreply.github.com>
2020-02-15 22:39:38 +0400

    Update 'repositories' section

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 allprojects {
     repositories {
         jcenter()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,7 @@ rootProject.name = "kotlinjs-npm-external"
 
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
-        mavenCentral()
-        maven(url = "https://plugins.gradle.org/m2/")
     }
 }
