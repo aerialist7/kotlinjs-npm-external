d548ccc81cde84441b05035cf949f519089d1873
aerialist7 <32072831+aerialist7@users.noreply.github.com>
2020-02-16 00:26:19 +0400

    KFC v.0.0.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("js") version "1.3.70-eap-184" apply false
-    id("com.github.turansky.kfc.root") version "0.0.24"
+    id("com.github.turansky.kfc.root") version "0.0.25"
 }
 
 allprojects {
