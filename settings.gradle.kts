167e580556ec75bdf267269fd610e98b7532c95d
aerialist7 <32072831+aerialist7@users.noreply.github.com>
2020-02-15 21:53:11 +0400

    settings.gradle -> settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "kotlinjs-npm-external"
+
+pluginManagement {
+    repositories {
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+}
